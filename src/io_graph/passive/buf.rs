47ceffd27f1368038a927e59a41e8b9a3b93658e
Astrid Yu <astrid@astrid.tech>
2026-05-07 08:25:34 -0700

    mpsc buf

--- src/io_graph/passive/buf.rs
+++ src/io_graph/passive/buf.rs
@@ -1,3 +1,9 @@
+use std::{
+    io::{Read, Write},
+    sync::mpsc,
+};
+
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 use ringbuf::{HeapCons, HeapProd, HeapRb, traits::Split};
 
 use crate::io_graph::{
@@ -6,8 +12,8 @@ use crate::io_graph::{
 };
 
 pub struct BufNode<'a> {
-    pub input: WriteJunction<'a, HeapProd<u8>>,
-    pub output: ReadJunction<'a, HeapCons<u8>>,
+    pub input: WriteJunction<'a, MpscWrite>,
+    pub output: ReadJunction<'a, MpscRead>,
     size: usize,
 }
 
@@ -31,5 +37,80 @@ impl<'a> Node<'a> for BufNode<'a> {
             inputs: vec![self.input.junction().clone()],
             outputs: vec![self.output.junction().clone()],
         }
+    }
+}
+
+pub struct MpscRead {
+    current: Bytes,
+
+    /// rx handle to sender end. Recv 0 bytes to signal EOF.
+    rx: mpsc::Receiver<Bytes>,
+}
+
+impl Read for MpscRead {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        // see if we need to read from the buffer
+        if self.current.is_empty() {
+            let msg = self.rx.recv().map_err(|_| {
+                std::io::Error::new(std::io::ErrorKind::BrokenPipe, "mpsc sender was dropped")
+            })?;
+
+            // 0 len means close
+            if msg.len() == 0 {
+                return Ok(0);
+            }
+            self.current = msg;
+        }
+
+        Ok(match self.current.try_copy_to_slice(buf) {
+            Ok(()) => buf.len(), // all successfully copied
+            Err(err) => err.available,
+        })
+    }
+}
+
+pub struct MpscWrite {
+    tx_limit: usize,
+    buf: BytesMut,
+
+    /// tx handle to receiver end. Send 0 bytes to signal EOF.
+    tx: mpsc::SyncSender<Bytes>,
+}
+
+impl MpscWrite {
+    fn close(self) {
+        self.tx.send(Bytes::new()).ok();
+    }
+}
+
+impl Write for MpscWrite {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        // calculate how many bytes we are writing
+        let max_write = self.tx_limit - self.buf.len();
+        let actual_write = buf.len().min(max_write);
+
+        // append to buffer
+        self.buf.put(&buf[..actual_write]);
+
+        // flush if we hit the limit
+        if self.buf.len() >= self.tx_limit {
+            self.flush()?;
+        }
+
+        Ok(actual_write)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        let msg = std::mem::take(&mut self.buf).freeze();
+
+        // don't send a 0, because that tells the rx that we're done
+        if msg.len() == 0 {
+            return Ok(());
+        }
+
+        self.tx.send(msg).map_err(|_| {
+            std::io::Error::new(std::io::ErrorKind::BrokenPipe, "mpsc receiver was dropped")
+        });
+        Ok(())
     }
 }
